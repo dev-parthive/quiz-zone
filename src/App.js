0a06f6fff60d5e2edb40d94ff48bbf093911cb13
dev-parthive <dev-parthive>
2022-10-15 12:07:11 +0600

    router setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './Layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {path: '/',
+    element: <Main></Main>
+  }
+  ])
   return (
     <div className="App">
+      <RouterProvider router={router
+      }></RouterProvider>
       
     </div>
   );
