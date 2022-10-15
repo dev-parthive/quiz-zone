7534ffb24e7d7db33a992e0835f9b4d686ae690e
dev-parthive <dev-parthive>
2022-10-15 16:16:25 +0600

    show quiz in home page done

---
+++ src/Components/Quiz/Quiz.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Quiz = ({quiz}) => {
+    const {id, logo, name, total } =quiz;
+    // console.log(quiz)
+    return (
+        <div className='quiz p-5 rounded shadow-lg  shadow-indigo-500/50'>
+            <img src={logo} className='w-11/12 mx-auto' alt="" />
+            <h2 className='text-2xl mt-3'>{quiz.name}</h2>
+            <div className='flex justify-between items-center w-11/12 mx-auto mt-2'>
+                <p>{total} Questions</p>
+                 <button className='btn btn-accent btn-outline'>
+                    <Link>Start quiz</Link>
+                 </button>
+                </div>
+        </div>
+    );
+};
+
+export default Quiz;
--- src/Components/Topics/Topic.js
+++ src/Components/Topics/Topic.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Quiz from '../Quiz/Quiz';
 
 const Topic = () => {
+    const topics = useLoaderData ()
+    const quizs = topics.data
     return (
-        <div>
-            <h2>This is topics</h2>
+        <div className=' w-9/12 mx-auto  mt-32 quizw-wraper grid    md:grid-cols-4  gap-5'>
+           {
+            quizs.map(quiz => <Quiz key={quiz.id} quiz={quiz}></Quiz>)
+           }
         </div>
     );
 };
